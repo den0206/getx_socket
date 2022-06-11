5c53736b29c79c0480cc2500a8da7399a9cd9ef7
yuuki sakai <s.yuuki0206@gmail.com>
2022-06-11 10:06:51 +0900

    status code 401

--- socket_server/src/middleware/check_auth.ts
+++ socket_server/src/middleware/check_auth.ts
@@ -10,12 +10,16 @@ export default function checkAuth(
   const bearerHeader = req.headers.authorization;
 
   if (bearerHeader) {
-    const secret = process.env.JWT_SECRET_KEY || 'mysecretkey';
-    const token = bearerHeader.split(' ')[1];
-    const decorded = jwt.verify(token, secret);
+    try {
+      const secret = process.env.JWT_SECRET_KEY || 'mysecretkey';
+      const token = bearerHeader.split(' ')[1];
+      const decorded = jwt.verify(token, secret);
 
-    res.locals.user = decorded;
-    next();
+      res.locals.user = decorded;
+      next();
+    } catch (e: any) {
+      return new ResponseAPI(res, {message: 'Token Expire'}).excute(403);
+    }
   } else {
     return new ResponseAPI(res, {message: 'No Token'}).excute(403);
   }
