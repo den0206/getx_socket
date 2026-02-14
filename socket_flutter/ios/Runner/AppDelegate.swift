75cc3f74bc505930e9bfc8a713e5993a85c32e02
den0206 <s.yuuki0206@gmail.com>
2026-02-14 10:00:22 +0900

    Flutter 3.41.1 対応

--- socket_flutter/ios/Runner/AppDelegate.swift
+++ socket_flutter/ios/Runner/AppDelegate.swift
@@ -3,14 +3,18 @@ import Flutter
 import Firebase
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-
-    FirebaseApp.configure()
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        FirebaseApp.configure()
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
+    // NEW: This is where plugin registration lives now
+    func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+        GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+    }
 }
+
